c209132a1947aef60d6ab840ad6ac64009921180
Eric Carmen Soto <ericcarmensoto@gmail.com>
2024-11-02 13:21:00 -0600

    Escenas ejemplo de manim

---
+++ example_scenes2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from manim import *
+
+
+class SquareToCircle(Scene):
+    def construct(self):
+        # Crear los objetos
+        circle = Circle()
+        square = Square()
+        
+        # Transformaciones del cuadrado
+        square.flip(RIGHT)
+        square.rotate(-3 * TAU / 8)
+        
+        # Establecer el relleno del círculo
+        circle.set_fill(PINK, opacity=0.5)
+
+        # Mostrar el cuadrado y luego transformarlo en círculo
+        self.play(Create(square)) 
+        self.play(Transform(square, circle))
+        self.play(FadeOut(square))
+
+
